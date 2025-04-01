e2441b55696cacc8be32df862bf3152e682c9e09
Keyurx11 <58322871+Keyurx11@users.noreply.github.com>
2025-04-01 22:54:10 +0100

    feat: enhance ticket purchase process with validation and payment integration

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -2,15 +2,28 @@ package uk.gov.dwp.uc.pairtest;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+import uk.gov.dwp.uc.pairtest.service.TicketOrderCalculator;
+import uk.gov.dwp.uc.pairtest.validation.TicketOrderValidator;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationService;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
+
+    private final TicketOrderValidator validator = new TicketOrderValidator();
+    private final TicketOrderCalculator calculator = new TicketOrderCalculator();
+    private final TicketPaymentService paymentService = new TicketPaymentServiceImpl();
+    private final SeatReservationService seatService = new SeatReservationServiceImpl();
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        validator.validate(accountId, ticketTypeRequests);
 
+        int totalAmount = calculator.calculateTotalAmount(ticketTypeRequests);
+        int seatsToReserve = calculator.calculateSeatsToReserve(ticketTypeRequests);
+
+        paymentService.makePayment(accountId, totalAmount);
+        seatService.reserveSeat(accountId, seatsToReserve);
     }
-
 }
